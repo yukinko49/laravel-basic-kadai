dbef1a47501448147f101160a5b03c22ef234354
yukinko49 <yk49.skrm@gmail.com>
2024-12-21 15:12:10 +0900

    コミット

---
+++ resources/views/posts/index.blad.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <titel>Laravel基礎</titel>
+  </head>
+
+  <body>
+    <h1>投稿一覧</h1>
+  </body>
+</html>
