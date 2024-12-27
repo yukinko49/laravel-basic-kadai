2dafc5804339b3bcfb39b9b8e729a96e2e160720
yukinko49 <yk49.skrm@gmail.com>
2024-12-27 16:01:02 +0900

    提出

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class PostController extends Controller
 {
     public function index(){
-        return view('posts.index');
+        $posts = DB::table('posts')->get();
+        return view('posts.index', compact('posts'));
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Laravel基礎</title>
 </head>
+
 <body>
   <h1>投稿一覧</h1>
+  <table>
+    <tr>
+
+      <th>タイトル</th>
+      <th>本文</th>
+
+    </tr>
+    @foreach($posts as $post)
+    <tr>
+      <td>{{ $post->text }}</td>
+      <td>{{ $post->content }}</td>
+    </tr>
+    @endforeach
+  </table>
 </body>
+
 </html>
